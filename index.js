aa17ffd74ff110b6931d5212d437c626fdb77d5a
Bryce DeSantis <potreader@hotmail.com>
2023-09-25 13:15:31 -0400

    Create a round of game logic

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === 'paper' && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection === computerSelection) {
+        return `Draw! You both picked ${playerSelection}`
+    } else {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+}
+
+const playerSelection = 'rock'
+const computerSelection = getComputerChoice()
+
+console.log(playRound(playerSelection, computerSelection))
